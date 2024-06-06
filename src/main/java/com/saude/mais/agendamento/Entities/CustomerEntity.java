587b6bedd46c92c0216a897fb910b35cd6ea5708
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-06 09:02:15 -0300

    Refactor CustomerEntity for improved data management and relationships
    - Updated CustomerEntity to use Lombok annotations for getters, setters, equals/hashCode, and toString methods
    - Introduced OneToOne relationship with UserEntity via user_id
    - Added OneToMany relationship with EmergencyContactsEntity for managing emergency contacts

--- src/main/java/com/saude/mais/agendamento/Entities/CustomerEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/CustomerEntity.java
@@ -1,91 +1,41 @@
 package com.saude.mais.agendamento.Entities;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.saude.mais.agendamento.Entities.User.UserEntity;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = "customers")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 public class CustomerEntity implements Serializable {
+
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "firt_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
+    @EqualsAndHashCode.Include
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    public UserEntity user;
 
-    private Short age;
-    private String gender;
 
-    @ManyToMany
-    @JoinTable(
-            name = "customer_address",
-            joinColumns = @JoinColumn(name = "customer_id"),
-            inverseJoinColumns = @JoinColumn(name = "address_id"))
-    private List<AddressEntity> address;
+    @OneToMany(mappedBy = "customer")
+    List<EmergencyContactsEntity> emergencyContacts;
 
-    public CustomerEntity() {}
 
-    public CustomerEntity(String firstName, String lastName, Short age, String gender) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.gender = gender;
-    }
 
-    public Long getId() {
-        return id;
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Short getAge() {
-        return age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public List<AddressEntity> getAddress() {
-        return address;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setAge(Short age) {
-        this.age = age;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CustomerEntity that = (CustomerEntity) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
+    public CustomerEntity(UserEntity user, List<EmergencyContactsEntity> emergencyContacts) {
+        this.user = user;
+        this.emergencyContacts = emergencyContacts;
     }
 }
