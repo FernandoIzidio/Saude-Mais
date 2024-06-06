e8d1896fd7b6bdf544334e3f4754166dc7739e1f
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-06 15:09:04 -0300

    Modify Entry Class

--- src/main/java/com/saude/mais/agendamento/AgendamentoApplication.java
+++ src/main/java/com/saude/mais/agendamento/AgendamentoApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.saude.mais.agendamento.Repositories")
 public class AgendamentoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AgendamentoApplication.class, args);
     }
-
 }
