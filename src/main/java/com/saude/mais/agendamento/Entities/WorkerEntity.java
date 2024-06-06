6accfbadfe6590a74964e48c86241f02c7800ccb
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-06 15:34:48 -0300

    Refactor WorkerEntity class
    - Added entity annotations for JPA mapping
    - Defined fields for firstName and lastName
    - Established Many-to-Many relationships with ExpertisesEntity and OfficeHoursEntity
    - Established One-to-One relationship with UserEntity
    - Established Many-to-Many relationship with AppointmentEntity

--- src/main/java/com/saude/mais/agendamento/Entities/WorkerEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/WorkerEntity.java
@@ -1,12 +1,72 @@
 package com.saude.mais.agendamento.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.saude.mais.agendamento.Entities.User.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "workers")
 public class WorkerEntity implements Serializable {
+
+    @EqualsAndHashCode.Include
+    @Setter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+
+    @Column(name = "first_name", nullable = false)
+    String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    String lastName;
+
+    @ManyToMany
+    @JoinTable(name = "worker_expertises",
+               joinColumns = @JoinColumn(name = "worker_id"),
+               inverseJoinColumns = @JoinColumn(name = "expertise_id")
+              )
+    List<ExpertisesEntity> expertises = new ArrayList<>();
+
+
+
+    @ManyToMany
+    @JoinTable(name = "worker_hours",
+               joinColumns = @JoinColumn(name = "worker_id"),
+               inverseJoinColumns = @JoinColumn(name = "hours_id")
+              )
+    List<OfficeHoursEntity> officeHours = new ArrayList<>();
+
+
+    @EqualsAndHashCode.Include
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    UserEntity user;
+
+    @ToString.Exclude
+    @JsonIgnore
+    @ManyToMany(mappedBy = "workers")
+    List<AppointmentEntity> appointments = new ArrayList<>();
+
+
+
+    public WorkerEntity(String firstName, String lastName, List<ExpertisesEntity> expertises, List<OfficeHoursEntity> officeHours, UserEntity user) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.expertises = expertises;
+        this.officeHours = officeHours;
+        this.user = user;
+    }
 
 }
