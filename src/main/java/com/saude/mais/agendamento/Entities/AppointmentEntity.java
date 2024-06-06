64a4611120a8a4c9b131bef10fdccad98daca136
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-06 10:23:23 -0300

    Create AppointmentEntity with JPA mapping and relationships
    - Defined AppointmentEntity with JPA annotations for entity mapping
    - Implemented fields: id, date, customer, workers, room, type, state, observations, diagnostic
    - Established ManyToOne relationships with CustomerEntity, RoomEntity, and HospitalEntity
    - Utilized Lombok for automatic generation of getters, setters, equals/hashCode, and toString methods

---
+++ src/main/java/com/saude/mais/agendamento/Entities/AppointmentEntity.java
@@ -0,0 +1,81 @@
+package com.saude.mais.agendamento.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@Entity
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "appointments")
+public class AppointmentEntity implements Serializable {
+
+
+    @Setter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Instant date;
+
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private CustomerEntity customer;
+
+
+    @Setter(AccessLevel.NONE)
+    @ManyToMany
+    @JoinTable(name = "appointment_worker",
+               joinColumns = @JoinColumn(name = "appointment_id"),
+               inverseJoinColumns = @JoinColumn(name = "worker_id")
+    )
+    private List<WorkerEntity> workers;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private RoomEntity room;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(length = 1000, nullable = false)
+    private String observations;
+
+    @Column(nullable = false)
+    private String diagnostic;
+
+
+
+
+    @Setter(AccessLevel.NONE)
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private HospitalEntity hospital;
+
+    public AppointmentEntity(Instant date, CustomerEntity customer, WorkerEntity worker, RoomEntity room, String type, String state, String observations, String diagnostic) {
+        this.date = date;
+        this.customer = customer;
+        this.room = room;
+        this.type = type;
+        this.state = state;
+        this.observations = observations;
+        this.diagnostic = diagnostic;
+    }
+
+
+}
