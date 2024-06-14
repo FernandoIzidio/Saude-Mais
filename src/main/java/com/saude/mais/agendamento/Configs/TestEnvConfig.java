1ab4ebe8ec139c595a853ac721c090d1df715daa
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-14 14:22:40 -0300

    Override method run() in TestEnvConfig class

--- src/main/java/com/saude/mais/agendamento/Configs/TestEnvConfig.java
+++ src/main/java/com/saude/mais/agendamento/Configs/TestEnvConfig.java
@@ -22,4 +22,15 @@ public class TestEnvConfig implements CommandLineRunner {
         this.userRepository = userRepository;
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Faker faker = new Faker();
+        List<UserEntity> users = new ArrayList<>(100);
+        for (int i = 0; i < 100; i++) {
+            UserEntity userEntity = new UserEntity(null, faker.name().firstName(), faker.internet().password(), faker.internet().safeEmailAddress(), false, false);
+            users.add(userEntity);
+        }
+        userRepository.saveAll(users);
+    }
 }
