6b51ff8fe1273e5b92a0c839773f50d2164a2f83
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-14 13:44:17 -0300

    Add basic fields and accessors to CustomerEntity

--- src/main/java/com/saude/mais/agendamento/Entities/CustomerEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/CustomerEntity.java
@@ -6,5 +6,61 @@ import java.util.Set;
 @Entity
 @Table(name = "customers")
 public class CustomerEntity {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
+        @Column(name = "firt_name")
+        private String firstName;
+        @Column(name = "last_name")
+        private String lastName;
+
+        private Short age;
+        private String gender;
+
+
+        public CustomerEntity() {}
+
+    public CustomerEntity(String firstName, String lastName, Short age, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
