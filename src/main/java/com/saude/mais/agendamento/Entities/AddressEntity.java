7ad282c5c25b3773f7b30405b8efeedbe9df25a9
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-14 13:31:00 -0300

    Add basic fields and acessors to AddressEntity

--- src/main/java/com/saude/mais/agendamento/Entities/AddressEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/AddressEntity.java
@@ -3,9 +3,74 @@ package com.saude.mais.agendamento.Entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
 
 
 @Table(name = "address")
 public class AddressEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+
+
+    public AddressEntity() {}
+
+    public AddressEntity(String street, String city, String state, String zip, String country) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
 
 }
