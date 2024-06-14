55da88a6448e6cb16e8caa6d8da668cb5714990a
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-14 13:56:08 -0300

    Create UserEntity class

---
+++ src/main/java/com/saude/mais/agendamento/Entities/UserEntity.java
@@ -0,0 +1,10 @@
+package com.saude.mais.agendamento.Entities;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name="tb_user")
+public class UserEntity implements Serializable {
+
+}
