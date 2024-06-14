b564fd933ccf27c2fc72fd3bd3285e676216a803
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-14 13:28:08 -0300

    Create AddressEntity class

---
+++ src/main/java/com/saude/mais/agendamento/Entities/AddressEntity.java
@@ -0,0 +1,11 @@
+package com.saude.mais.agendamento.Entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+
+@Table(name = "address")
+public class AddressEntity implements Serializable {
+
+}
