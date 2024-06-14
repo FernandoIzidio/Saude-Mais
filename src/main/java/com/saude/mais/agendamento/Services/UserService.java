ec7364b087bda281bf608e59e8da09fb7bd3ede7
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-14 14:10:11 -0300

    Create UserService class

---
+++ src/main/java/com/saude/mais/agendamento/Services/UserService.java
@@ -0,0 +1,14 @@
+package com.saude.mais.agendamento.Services;
+
+import com.saude.mais.agendamento.Entities.UserEntity;
+import com.saude.mais.agendamento.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+
+}
