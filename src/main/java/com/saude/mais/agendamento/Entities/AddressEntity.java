075bfa2dfb78662f55b341562ea81f5eeeb5af4a
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-14 13:33:26 -0300

    Implement equals and hashCode methods

--- src/main/java/com/saude/mais/agendamento/Entities/AddressEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/AddressEntity.java
@@ -74,5 +74,16 @@ public class AddressEntity implements Serializable {
         this.country = country;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AddressEntity address = (AddressEntity) o;
+        return Objects.equals(id, address.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
