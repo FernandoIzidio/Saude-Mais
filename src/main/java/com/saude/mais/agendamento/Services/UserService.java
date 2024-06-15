bce353c55eb5b02e74bb74fcc63538cf15e8da5a
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-15 15:44:54 -0300

    Refactor UserService class
    - Added findAll method to retrieve all users
    - Added findById method to retrieve user by ID
    - Added findByCpf method to retrieve user by CPF
    - Added findByEmail method to retrieve user by email
    - Added save method to save user entity
    - Added deleteById and delete methods to remove user entity

--- src/main/java/com/saude/mais/agendamento/Services/UserService.java
+++ src/main/java/com/saude/mais/agendamento/Services/UserService.java
@@ -1,8 +1,9 @@
 package com.saude.mais.agendamento.Services;
 
-import com.saude.mais.agendamento.Entities.UserEntity;
+import com.saude.mais.agendamento.Entities.User.UserEntity;
 import com.saude.mais.agendamento.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,5 +20,27 @@ public class UserService {
 
     public List<UserEntity> findAll() {
         return userRepository.findAll();
+    }
+
+    public UserEntity findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public UserEntity findByCpf(String cpf) {
+        return userRepository.findByCpf(cpf);
+    }
+
+    public UserDetails findByEmail(String email) { return userRepository.findByEmail(email); }
+
+    public UserEntity save(UserEntity user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public void delete(UserEntity user) {
+        userRepository.delete(user);
     }
 }
