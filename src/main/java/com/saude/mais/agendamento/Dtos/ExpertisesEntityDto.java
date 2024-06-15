261e5c8bac73367b17c1dc7dc65b4b97b7b5db68
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-15 16:11:47 -0300

    Create DTO record for ExpertisesEntity
    - Define DTO field 'name' to represent ExpertisesEntity data

---
+++ src/main/java/com/saude/mais/agendamento/Dtos/ExpertisesEntityDto.java
@@ -0,0 +1,9 @@
+package com.saude.mais.agendamento.Dtos;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.saude.mais.agendamento.Entities.ExpertisesEntity}
+ */
+public record ExpertisesEntityDto(String name) implements Serializable {
+}
