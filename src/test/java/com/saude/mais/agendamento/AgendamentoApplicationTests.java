f1c0480e3b0ac84a87681bfae74b311b13cc9712
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-15 17:25:45 -0300

    Refactor AgendamentoApplicationTests with phoneValidatorServiceLoads test
    - Added phoneValidatorServiceLoads test to ensure PhoneValidatorService is loaded in the Spring context.

--- src/test/java/com/saude/mais/agendamento/AgendamentoApplicationTests.java
+++ src/test/java/com/saude/mais/agendamento/AgendamentoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.saude.mais.agendamento;
 
+import com.saude.mais.agendamento.Services.PhoneValidatorService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @SpringBootTest
 class AgendamentoApplicationTests {
 
+    private PhoneValidatorService phoneValidatorService;
+
+    @Autowired
+    public AgendamentoApplicationTests( PhoneValidatorService phoneValidatorService ){
+        this.phoneValidatorService = phoneValidatorService;
+    }
+
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void phoneValidatorServiceLoads() {
+
+        assertNotNull(phoneValidatorService, "PhoneValidatorService should be loaded in the context");
+    }
+
 
 }
