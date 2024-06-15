cb5263231ae395ca5886994eae38226a2d990a65
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-15 19:34:50 -0300

    Create JavaScript event listener for hospital registration form
    - Implemented event listener in main.js to handle changes in the hospital zip code input.
    - Utilizes AddressForms.getAddress() to fetch address details asynchronously.
    - Updates hospital street, city, and state fields based on the fetched address data.
    - Integrated error handling to display a user-friendly error message via renderErrorFriendly() if address fetching fails.

---
+++ src/main/resources/static/js/hospitalAuth.js
@@ -0,0 +1,21 @@
+import { AddressForms } from "./Util/formUtils.js";
+import { renderErrorFriendly } from "./Util/ErrorFriendly.js";
+
+document.addEventListener("DOMContentLoaded", event => {
+    AddressForms.renderStateOptions("hospitalState");
+
+    const cepInput = document.getElementById("hospitalZip");
+
+    cepInput.addEventListener("change", async event => {
+        try {
+            const address = await AddressForms.getAddress(event.target.value);
+            document.getElementById("hospitalStreet").value = address.logradouro;
+            document.getElementById("hospitalCity").value = address.localidade;
+            document.getElementById("hospitalState").value = AddressForms.STATE_DICTIONARY[address.uf];
+
+
+        } catch (err) {
+            renderErrorFriendly("hospitalZip", "Erro ao buscar cep", "div");
+        }
+    });
+});
