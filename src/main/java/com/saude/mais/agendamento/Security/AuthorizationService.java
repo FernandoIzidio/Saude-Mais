737469a6abbcaf902164200df9bf57534e1b835b
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-15 15:48:55 -0300

    Create AuthorizationService class
    - Implements UserDetailsService for user authentication
    - Adds loadUserByUsername method to load user details by email
    - Injects UserRepository dependency

---
+++ src/main/java/com/saude/mais/agendamento/Security/AuthorizationService.java
@@ -0,0 +1,25 @@
+package com.saude.mais.agendamento.Security;
+
+import com.saude.mais.agendamento.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public AuthorizationService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
+
+}
