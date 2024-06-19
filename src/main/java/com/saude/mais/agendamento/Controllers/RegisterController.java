64b195f4fb8f203ad4a3319bf8cfcd9f91ea2eca
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-19 16:35:55 -0300

    Refactor RegisterController -- Remove endpoint /register/hospital and /register/customer -- Add subdomain redirecting feature

--- src/main/java/com/saude/mais/agendamento/Controllers/RegisterController.java
+++ src/main/java/com/saude/mais/agendamento/Controllers/RegisterController.java
@@ -4,68 +4,89 @@ import com.saude.mais.agendamento.Dtos.AddressEntityDto;
 import com.saude.mais.agendamento.Dtos.HospitalEntityDto;
 import com.saude.mais.agendamento.Dtos.HospitalRegisterDto;
 import com.saude.mais.agendamento.Dtos.UserEntityDto;
-import com.saude.mais.agendamento.Entities.AddressEntity;
 import com.saude.mais.agendamento.Entities.HospitalEntity;
-import com.saude.mais.agendamento.Entities.User.UserEntity;
 import com.saude.mais.agendamento.Entities.User.UserRole;
+import com.saude.mais.agendamento.Services.AddressService;
+import com.saude.mais.agendamento.Services.HospitalService;
 import com.saude.mais.agendamento.Services.RegistrationService;
+import com.saude.mais.agendamento.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.time.Instant;
 import java.time.ZoneId;
 
 @Controller
-@RequestMapping("/register")
+@RequestMapping(value = "/register")
 public class RegisterController {
 
+    private AddressService addressService;
+    private UserService userService;
     private RegistrationService registrationService;
+    private HospitalService hospitalService;
 
     @Autowired
-    public RegisterController(RegistrationService registrationService) {
+    public RegisterController(RegistrationService registrationService, AddressService addressService, UserService userService, HospitalService hospitalService) {
         this.registrationService = registrationService;
+        this.addressService = addressService;
+        this.userService = userService;
+        this.hospitalService = hospitalService;
     }
 
-
-    @GetMapping("/customer")
-    public String getCustomerTemplate() {
-        return "customer_auth";
-    }
-
-    @GetMapping("/hospital")
+    @GetMapping
     public String getHospitalTemplate(Model model) {
 
-        UserEntityDto userEntityDto = new UserEntityDto(null, null, null, null, null, null, null, UserRole.ADMIN, null, null);
-        AddressEntityDto address = new AddressEntityDto(null, null, null, null);
-        HospitalEntityDto hospitalDto = new HospitalEntityDto(null, null, address, null, null, null, null, null, null, null, null);
+        UserRole userRole = UserRole.ADMIN;
+        UserEntityDto userEntityDto = userService.createNullUserDto(userRole);
+
+        AddressEntityDto address = addressService.createNullAddressDto();
+
+        HospitalEntityDto hospitalDto = hospitalService.createNullHospitalDto(address);
+
         HospitalRegisterDto hospitalRegisterDto = new HospitalRegisterDto(hospitalDto, userEntityDto);
+
         model.addAttribute("hospitalForm", hospitalRegisterDto);
         return "hospital_auth";
     }
 
-    @PostMapping("/hospital")
-    public String registerHospital(@ModelAttribute HospitalRegisterDto hospital) {
+    @PostMapping
+    public RedirectView registerHospital(@ModelAttribute HospitalRegisterDto hospital) {
         UserEntityDto userEntityDto = hospital.userEntityDto();
         HospitalEntityDto hospitalEntityDto = hospital.hospitalEntityDto();
+        AddressEntityDto address = hospitalEntityDto.address();
 
         Instant licenseExpirationDate = hospitalEntityDto.licenseExpirationDate().atStartOfDay(ZoneId.systemDefault()).toInstant();
 
-        Instant birhtdate = userEntityDto.birthDate().atStartOfDay(ZoneId.systemDefault()).toInstant();
+        String hospitalName = hospitalEntityDto.domainName().trim().replaceAll("\\s+", "").toLowerCase();
 
-        UserEntity userEntity = new UserEntity(userEntityDto.firstName(), userEntityDto.lastName(), userEntityDto.username(), userEntityDto.password(), userEntityDto.email(), userEntityDto.phone(), userEntityDto.cpf(), userEntityDto.role(), birhtdate);
+        String domain = "www." + hospitalName + ".saude-mais.com.br";
+        HospitalEntity hospitalEntity = hospitalService.createHospitalEntity(hospitalEntityDto, domain, addressService.createAddressEntity(address), licenseExpirationDate);
 
-        AddressEntity address = new AddressEntity(hospitalEntityDto.address().street(), hospitalEntityDto.address().city(), hospitalEntityDto.address().state(), hospitalEntityDto.address().zip());
+        String url = "http://" + domain + ":8080/";
 
 
-        HospitalEntity hospitalEntity = new HospitalEntity(hospitalEntityDto.name(), hospitalEntityDto.cnpj(), address, hospitalEntityDto.primaryPhone(), hospitalEntityDto.secondaryPhone(), hospitalEntityDto.email(), hospitalEntityDto.bedCapacity(), hospitalEntityDto.employeeCount(), hospitalEntityDto.doctorCount(), hospitalEntityDto.sanitaryLicenseNumber(), licenseExpirationDate);
 
-        registrationService.registerHospital(hospitalEntity, userEntity);
-        System.out.println("Hospital created with success: " + hospitalEntity.toString());
-        return "redirect:/";
+        String scriptPath = "src/main/java/com/saude/mais/agendamento/Scripts/tenantCreator.sh";
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(scriptPath, hospitalName);
+
+            Process process = processBuilder.start();
+
+            int exitCode = process.waitFor();
+
+            registrationService.registerHospital(hospitalEntity, userEntityDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        RedirectView redirectView = new RedirectView(url);
+        return redirectView;
     }
 }
