388b3c92c9f9c783d6e306f8b49170340757ae07
FernandoIzidio <luizfernando1010200@gmail.com>
2024-06-19 16:50:34 -0300

    Refactor AuthorizationService to improve UserDetailsService implementation

--- src/main/java/com/saude/mais/agendamento/Security/AuthorizationService.java
+++ src/main/java/com/saude/mais/agendamento/Security/AuthorizationService.java
@@ -1,5 +1,6 @@
 package com.saude.mais.agendamento.Security;
 
+import com.saude.mais.agendamento.Entities.User.UserEntity;
 import com.saude.mais.agendamento.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,16 +11,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthorizationService implements UserDetailsService {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public AuthorizationService(UserRepository userRepository){
+    public AuthorizationService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), user.getAuthorities());
     }
-
 }
