f7e4c34fcb06bb7eca2ba527acd53faef05c79e5
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:58:18 -0300

    Update MedicationEntity.java
    - Removed @EqualsAndHashCode annotation and added custom equals() and hashCode() methods.

--- src/main/java/com/saude/mais/agendamento/Entities/MedicationEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/MedicationEntity.java
@@ -3,6 +3,7 @@ package com.saude.mais.agendamento.Entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -10,7 +11,6 @@ import java.util.Objects;
 
 @Getter
 @Setter
-@EqualsAndHashCode(of = "id")
 @ToString(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
@@ -99,5 +99,19 @@ public class MedicationEntity implements Serializable {
         this.appointment = appointment;
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        MedicationEntity that = (MedicationEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
 
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
