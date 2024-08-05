040fb07f6a1adee6086efaef19a11c8ec98f0a68
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:43:05 -0300

    Rename CustomerEntityDto to CustomerDto
    - Renamed CustomerEntityDto to CustomerDto.
    - Changed UserEntityDto to UserDto.
    - Added validation for user and hospitalId.
    - Added static method to convert EmergencyContactsDto to EmergencyContactsEntity.

--- src/main/java/com/saude/mais/agendamento/Dtos/CustomerEntityDto.java
+++ src/main/java/com/saude/mais/agendamento/Dtos/CustomerEntityDto.java
@@ -1,11 +1,23 @@
 package com.saude.mais.agendamento.Dtos;
 
+import com.saude.mais.agendamento.Entities.EmergencyContactsEntity;
+import jakarta.validation.constraints.NotNull;
+
 import java.io.Serializable;
 import java.util.List;
 
 /**
  * DTO for {@link com.saude.mais.agendamento.Entities.CustomerEntity}
  */
-public record CustomerEntityDto(UserEntityDto user,
-                                List<EmergencyContactsEntityDto> emergencyContacts) implements Serializable {
+public record CustomerEntityDto(
+                                @NotNull(message = "Usuario é obrigatório")
+                                UserDto user,
+                                @NotNull(message = "Id de hospital é obrigatório")
+                                Long hospitalId,
+                                List<EmergencyContactsDto> emergencyContacts) implements Serializable {
+
+    public static List<EmergencyContactsEntity> toEmergencyContactsEntities(List<EmergencyContactsDto> emergencyContactsDtos) {
+        return emergencyContactsDtos.stream().map(EmergencyContactsDto::toEmergencyContactsEntity).toList();
+    }
+
 }
