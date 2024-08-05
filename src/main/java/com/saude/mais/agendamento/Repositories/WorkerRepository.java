c210b73c223daf8d2093950e86962f040ef4eb66
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 18:13:14 -0300

    Comment out findAllByHospitalId query in WorkerRepository

--- src/main/java/com/saude/mais/agendamento/Repositories/WorkerRepository.java
+++ src/main/java/com/saude/mais/agendamento/Repositories/WorkerRepository.java
@@ -1,7 +1,16 @@
 package com.saude.mais.agendamento.Repositories;
 
+import com.saude.mais.agendamento.Entities.HospitalEntity;
 import com.saude.mais.agendamento.Entities.WorkerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WorkerRepository extends JpaRepository<WorkerEntity, Long> {
+
+//    @Query("SELECT w FROM WorkerEntity w WHERE w.user.hospital.id = :hospitalId")
+//    List<WorkerEntity> findAllByHospitalId(@Param("hospitalId") Long hospitalId);
+
 }
