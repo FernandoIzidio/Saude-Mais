1e4d47bc4b81119a8333f3cdf4c7d75965eda4ba
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 18:12:45 -0300

    Add findByCustomer_Id method to AppointmentRepository

--- src/main/java/com/saude/mais/agendamento/Repositories/AppointmentRepository.java
+++ src/main/java/com/saude/mais/agendamento/Repositories/AppointmentRepository.java
@@ -4,4 +4,5 @@ import com.saude.mais.agendamento.Entities.AppointmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRepository extends JpaRepository<AppointmentEntity, Long> {
+    AppointmentEntity findByCustomer_Id(Long customerId);
 }
