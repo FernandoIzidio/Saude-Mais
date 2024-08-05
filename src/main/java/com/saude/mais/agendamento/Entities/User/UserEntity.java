59081428902486b1134aec080537f0b9008aa0a9
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 18:00:23 -0300

    Update UserEntity.java
    - Added new fields: gender, userRole, birthdate, addresses, and hospitals.
    - Updated constructors to reflect new fields.
    - Modified getAuthorities() to use userRole instead of role.
    - Added getAge() method to calculate age based on birthdate.
    - Implemented conversion methods to UserDto, AddressDto, and HospitalDto.
    - Removed @EqualsAndHashCode annotation and added custom equals() and hashCode() methods.

--- src/main/java/com/saude/mais/agendamento/Entities/User/UserEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/User/UserEntity.java
@@ -1,28 +1,35 @@
 package com.saude.mais.agendamento.Entities.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.saude.mais.agendamento.Dtos.AddressDto;
+import com.saude.mais.agendamento.Dtos.HospitalDto;
+import com.saude.mais.agendamento.Dtos.UserDto;
 import com.saude.mais.agendamento.Entities.AddressEntity;
 import com.saude.mais.agendamento.Entities.HospitalEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
-import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name="tb_user")
 @Getter
 @Setter
 @NoArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
 @ToString
 public class UserEntity implements Serializable, UserDetails {
 
-    @EqualsAndHashCode.Include
+
     @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,44 +41,50 @@ public class UserEntity implements Serializable, UserDetails {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @EqualsAndHashCode.Include
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+
     @Column(nullable = false, name = "username", unique = true)
     private String user;
 
     @Column(nullable = false)
     private String password;
 
-    @EqualsAndHashCode.Include
+
     @Setter(AccessLevel.NONE)
     @Column(nullable = false, unique = true)
     private String email;
 
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 11)
     private String phone;
 
-    @EqualsAndHashCode.Include
+
     @Setter(AccessLevel.NONE)
     @Column(nullable = false, unique = true, length = 11)
     private String cpf;
 
 
     @Column(nullable = false)
-    private UserRole role;
+    private UserRole userRole;
 
     @Column(name = "birth_date", nullable = false)
-    private Instant birthDate;
+    private LocalDate birthdate;
 
 
 
     @Setter(AccessLevel.NONE)
     @ManyToMany
-        @JoinTable(
+    @JoinTable(
             name = "customer_address",
             joinColumns = @JoinColumn(name = "customer_id"),
             inverseJoinColumns = @JoinColumn(name = "address_id")
-        )
-    private List<AddressEntity> address = new ArrayList<>();
+    )
+    private List<AddressEntity> addresses = new ArrayList<>();
+
+
 
     @Setter(AccessLevel.NONE)
     @ManyToMany
@@ -81,57 +94,126 @@ public class UserEntity implements Serializable, UserDetails {
     )
     List<HospitalEntity> hospitals = new ArrayList<>();
 
-    
-    public UserEntity(String firstName, String lastName, String username, String password, String email, String phone, String cpf, UserRole role, Instant birthDate) {
+
+    public UserEntity(String firstName, String lastName, Gender gender, String username, String password, String email, String phone, String cpf, UserRole userRole, LocalDate birthdate, List<AddressEntity> addresses, List<HospitalEntity> hospitals) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.gender = gender;
         this.user = username;
         this.password = password;
         this.email = email;
         this.phone = phone;
         this.cpf = cpf;
-        this.role = role;
-        this.birthDate = birthDate;
+        this.userRole = userRole;
+        this.birthdate = birthdate;
+        this.addresses = addresses;
+        this.hospitals = hospitals;
     }
 
+    public UserEntity(String firstName, String lastName, Gender gender, String username, String password, String email, String phone, String cpf, UserRole userRole, LocalDate birthdate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.user = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.cpf = cpf;
+        this.userRole = userRole;
+        this.birthdate = birthdate;
+    }
+
+    @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_WORKER"), new SimpleGrantedAuthority("ROLE_CUSTOMER"));
+        if (this.userRole == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_WORKER"), new SimpleGrantedAuthority("ROLE_CUSTOMER"));
 
-        if (this.role == UserRole.WORKER) return List.of(new SimpleGrantedAuthority("ROLE_WORKER"));
+        if (this.userRole == UserRole.WORKER) return List.of(new SimpleGrantedAuthority("ROLE_WORKER"));
 
         return List.of(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
     }
 
+    
     @Override
     public String getPassword() {
         return password;
     }
 
-    @Override
-    public String getUsername() {
-        return email;
+    public Integer getAge(){
+        return Period.between(this.birthdate, LocalDate.now()).getYears();
     }
 
+    @JsonIgnore
+    @Override
+    public String getUsername() {
+        return user;
+    }
+
+
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
-
-
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public UserDto toUserDto() {
+        return new UserDto(this.id, this.firstName, this.lastName, this.gender, this.user, this.password, this.email, this.phone, this.cpf, this.userRole, this.birthdate, toAddressDtoList(), toHospitalDtoList()).prettyData();
+    }
+
+
+    public List<AddressDto> toAddressDtoList(){
+        List<AddressDto> addresses = new ArrayList();
+
+        getAddresses().forEach((address) -> {
+            addresses.add(address.toAddressDto().prettyData());
+        });
+
+        return addresses;
+    }
+
+
+    public List<HospitalDto> toHospitalDtoList(){
+        List<HospitalDto> hospitals = new ArrayList();
+
+        getHospitals().forEach((hospital) -> {
+            hospitals.add(hospital.toHospitalDto().prettyData());
+        });
+
+        return hospitals;
+    }
+
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        UserEntity that = (UserEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
