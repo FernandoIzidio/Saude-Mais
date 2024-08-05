7b8ae27fccbda2346bf311c5ba263d0c3776ec5f
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:45:07 -0300

    Rename RoomEntityDto to RoomDto
    - Renamed RoomEntityDto to RoomDto.

--- src/main/java/com/saude/mais/agendamento/Dtos/RoomDto.java
+++ src/main/java/com/saude/mais/agendamento/Dtos/RoomDto.java
@@ -5,5 +5,5 @@ import java.io.Serializable;
 /**
  * DTO for {@link com.saude.mais.agendamento.Entities.RoomEntity}
  */
-public record RoomEntityDto(String number, String type) implements Serializable {
+public record RoomDto(String number, String type) implements Serializable {
 }
