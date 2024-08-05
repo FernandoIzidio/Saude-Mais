9446a35a7d1a6d1985b0a18ede94c2b0c44d0606
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:57:03 -0300

    Update ExpertisesEntity.java
    - Removed @EqualsAndHashCode annotation and added custom equals() and hashCode() methods.
    - Updated constructor to remove id parameter.

--- src/main/java/com/saude/mais/agendamento/Entities/ExpertisesEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/ExpertisesEntity.java
@@ -3,6 +3,7 @@ package com.saude.mais.agendamento.Entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serializable;
 import java.util.List;
@@ -10,13 +11,13 @@ import java.util.Objects;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "expertises")
 public class ExpertisesEntity implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
@@ -27,7 +28,6 @@ public class ExpertisesEntity implements Serializable {
 
     @Setter(AccessLevel.NONE)
     @ToString.Exclude
-    @EqualsAndHashCode.Exclude
     @JsonIgnore
     @ManyToMany(mappedBy = "expertises")
     private List<WorkerEntity> doctors;
@@ -37,10 +37,24 @@ public class ExpertisesEntity implements Serializable {
     @JoinColumn(name = "hospital_id")
     private HospitalEntity hospital;
 
-    public ExpertisesEntity(Long id, String name) {
-        this.id = id;
+    public ExpertisesEntity( String name) {
         this.name = name;
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ExpertisesEntity that = (ExpertisesEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
 
