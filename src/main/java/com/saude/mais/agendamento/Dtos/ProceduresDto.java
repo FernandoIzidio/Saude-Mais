edc6c79adac2a6a4bc2be9f483103cbd08714e19
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:44:58 -0300

    Rename ProceduresEntityDto to ProceduresDto
    - Renamed ProceduresEntityDto to ProceduresDto.

--- src/main/java/com/saude/mais/agendamento/Dtos/ProceduresDto.java
+++ src/main/java/com/saude/mais/agendamento/Dtos/ProceduresDto.java
@@ -6,5 +6,5 @@ import java.time.Instant;
 /**
  * DTO for {@link com.saude.mais.agendamento.Entities.ProceduresEntity}
  */
-public record ProceduresEntityDto(String name, Instant date, String duration, String result) implements Serializable {
+public record ProceduresDto(String name, Instant date, String duration, String result) implements Serializable {
 }
