c59ec8abf0eb527ac2b1acc7d55c4667b27faf94
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:59:06 -0300

    Update RoomEntity.java
    - Removed @EqualsAndHashCode annotation and added custom equals() and hashCode() methods.

--- src/main/java/com/saude/mais/agendamento/Entities/RoomEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/RoomEntity.java
@@ -2,6 +2,7 @@ package com.saude.mais.agendamento.Entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -12,7 +13,6 @@ import java.util.Objects;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
 @ToString
 public class RoomEntity implements Serializable {
 
@@ -36,5 +36,21 @@ public class RoomEntity implements Serializable {
     public RoomEntity(String number, String type) {
         this.number = number;
         this.type = type;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        RoomEntity that = (RoomEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
