4d8bc645093490f6ff27f4dbefbad763ab32d852
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:14:07 -0300

    Refactor test environment configuration
    - Add imports for `BrazilianStates`, `Gender`, and `UserService`.
    - Update class constructor to include `UserService`.
    - Refactor `AddressEntity` and `HospitalEntity` creation with new values and logic.
    - Add creation and saving of a `UserEntity`, linking with addresses and hospitals.
    - Update `generatePhoneNumber` method to generate valid phone numbers.
    - Remove unnecessary commented code.

--- src/main/java/com/saude/mais/agendamento/Configs/TestEnvConfig.java
+++ src/main/java/com/saude/mais/agendamento/Configs/TestEnvConfig.java
@@ -2,18 +2,22 @@ package com.saude.mais.agendamento.Configs;
 
 import com.github.javafaker.Faker;
 import com.saude.mais.agendamento.Entities.AddressEntity;
+import com.saude.mais.agendamento.Entities.BrazilianStates;
 import com.saude.mais.agendamento.Entities.HospitalEntity;
+import com.saude.mais.agendamento.Entities.User.Gender;
 import com.saude.mais.agendamento.Entities.User.UserEntity;
 import com.saude.mais.agendamento.Entities.User.UserRole;
 import com.saude.mais.agendamento.Repositories.AddressRepository;
 import com.saude.mais.agendamento.Repositories.HospitalRepository;
 import com.saude.mais.agendamento.Repositories.UserRepository;
+import com.saude.mais.agendamento.Services.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.time.Instant;
+import java.time.*;
 import java.util.*;
 
 @Configuration
@@ -21,14 +25,16 @@ import java.util.*;
 public class TestEnvConfig implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final UserService userService;
     private  HospitalRepository hospitalRepository;
     private  AddressRepository addressRepository;
 
     @Autowired
-    public TestEnvConfig(UserRepository userRepository, HospitalRepository hospitalRepository, AddressRepository addressRepository) {
+    public TestEnvConfig(UserRepository userRepository, HospitalRepository hospitalRepository, AddressRepository addressRepository, UserService userService) {
         this.userRepository = userRepository;
         this.hospitalRepository = hospitalRepository;
         this.addressRepository = addressRepository;
+        this.userService = userService;
     }
 
 
@@ -42,16 +48,41 @@ public class TestEnvConfig implements CommandLineRunner {
         List<AddressEntity> addressEntities = new ArrayList<>();
         List<HospitalEntity> hospitalEntities = new ArrayList<>();
 
+        UserEntity user = null;
+        HospitalEntity hospitalEntity = null;
         for (int i = 0; i < 10; i++) {
-            AddressEntity address = new AddressEntity(faker.address().streetName(), faker.address().city(), faker.address().state(), faker.address().zipCode());
+            AddressEntity address = new AddressEntity(faker.address().streetName(), faker.address().streetName(), faker.address().streetAddressNumber(), faker.address().city(), BrazilianStates.MINAS_GERAIS, faker.address().zipCode());
             addressEntities.add(address);
 
-            HospitalEntity hospital = new HospitalEntity(faker.medical().hospitalName(), generateValidCnpj(), address, generateCellPhoneNumber(), generateCellPhoneNumber(), faker.internet().safeEmailAddress(), faker.number().numberBetween(10, 50),  faker.number().numberBetween(10, 25), faker.number().numberBetween(5, 20), String.valueOf(faker.idNumber()),  faker.date().birthday().toInstant());
+            String hospitalName = faker.internet().domainName().trim().replaceAll("\\s+", "").replace(".", "").toLowerCase();
+
+            String domain = "www." + hospitalName + ".saude-mais.com.br";
+
+            HospitalEntity hospital = new HospitalEntity(faker.medical().hospitalName(), generateValidCnpj(), domain, address, generatePhoneNumber(false), generatePhoneNumber(false), faker.internet().safeEmailAddress());
+
+
             hospitalEntities.add(hospital);
         }
 
         addressRepository.saveAll(addressEntities);
         hospitalRepository.saveAll(hospitalEntities);
+
+        var bcrypt = new BCryptPasswordEncoder();
+        LocalDate birthdate = faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        AddressEntity addresses = new AddressEntity("Rua Maria Aparecida Peixoto", "Santa Rosa", "59", "Uberlândia", BrazilianStates.MINAS_GERAIS, "38401312");
+
+        hospitalEntity = new HospitalEntity("Albert Einstein", "75796109000186", "www.alberteinstein.saude-mais.com.br", addresses, "3498245817", "3498245818", "albert@email.com");
+
+        user = new UserEntity("Tulio", "Alves", Gender.MASCULINO, "tulio123", bcrypt.encode("123"), "tulio@email.com", generatePhoneNumber(true),"17435731625", UserRole.ADMIN, birthdate);
+
+
+        user.getAddresses().add(addresses);
+        user.getHospitals().add(hospitalEntity);
+        hospitalEntity.setAddress(addresses);
+        addressRepository.save(addresses);
+        hospitalRepository.save(hospitalEntity);
+        userRepository.save(user);
     }
 
     public String generateRandomCPF() {
@@ -110,24 +141,19 @@ public class TestEnvConfig implements CommandLineRunner {
         return verifier > 9 ? 0 : verifier;
     }
 
-    public static String generateCellPhoneNumber() {
+    public static String generatePhoneNumber(Boolean cellphone) {
         Random random = new Random();
-        StringBuilder phoneNumber = new StringBuilder("9");
+
+        StringBuilder phoneNumber = new StringBuilder();
 
         for (int i = 1; i < 11; i++) {
-            phoneNumber.append(random.nextInt(10)); // Gera dígitos de 0 a 9
+            phoneNumber.append(random.nextInt(10));
         }
+        if (cellphone) {
+            phoneNumber.insert(2, 9);
+        }
+
         return phoneNumber.toString();
     }
 
 }
-
-//            String role = roles[faker.random().nextInt(0, 2)];
-//            UserRole userRole =  role.equals("admin") ? UserRole.ADMIN : role.equals("worker") ? UserRole.WORKER : UserRole.CUSTOMER;
-//            do {
-//                cpf = generateRandomCPF();
-//                existingUser = userRepository.findByCpf(cpf);
-//            } while (existingUser != null);
-//
-//            UserEntity userEntity = new UserEntity(null, faker.name().firstName(), faker.name().lastName(), faker.name().username(), faker.internet().password(), faker.internet().safeEmailAddress(), faker.phoneNumber().cellPhone(), cpf, userRole, faker.date().birthday().toInstant());
-//            users.add(userEntity);
