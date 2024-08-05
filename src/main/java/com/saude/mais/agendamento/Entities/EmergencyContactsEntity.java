f5685cbbf46198b378321d9e5c6f3557b795c779
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:55:40 -0300

    Update EmergencyContactsEntity.java
    - Removed @EqualsAndHashCode annotation, and added custom equals() and hashCode() methods.
    - Added @JsonIgnore to customer field and updated constructor to exclude customer.

--- src/main/java/com/saude/mais/agendamento/Entities/EmergencyContactsEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/EmergencyContactsEntity.java
@@ -1,7 +1,9 @@
 package com.saude.mais.agendamento.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Objects;
 
@@ -11,11 +13,10 @@ import java.util.Objects;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 public class EmergencyContactsEntity {
 
-    @EqualsAndHashCode.Include
+
     @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,14 +28,14 @@ public class EmergencyContactsEntity {
     @Column(name = "last_name", nullable = false)
     String lastName;
 
-    @EqualsAndHashCode.Include
+
     @Column(length = 11, nullable = false)
     String phone;
 
     @Column(nullable = false)
     String relationship;
 
-
+    @JsonIgnore
     @ToString.Exclude
     @Setter(AccessLevel.NONE)
     @ManyToOne
@@ -46,11 +47,26 @@ public class EmergencyContactsEntity {
     @JoinColumn(name = "hospital_id")
     private HospitalEntity hospital;
 
-    public EmergencyContactsEntity(String firstName, String lastName, String phone, String relationship, CustomerEntity customer) {
+    public EmergencyContactsEntity(String firstName, String lastName, String phone, String relationship) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phone = phone;
         this.relationship = relationship;
-        this.customer = customer;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        EmergencyContactsEntity that = (EmergencyContactsEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
