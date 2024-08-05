965273418be394ac1e815a3c50161cbdfe1d8ad9
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 17:54:14 -0300

    Update AddressEntity.java
    - Added fields: neighborhood, number.
    - Changed state type to BrazilianStates.
    - Updated constructor and added toAddressDto() method.
    - Updated equals() and hashCode() methods for HibernateProxy compatibility.

--- src/main/java/com/saude/mais/agendamento/Entities/AddressEntity.java
+++ src/main/java/com/saude/mais/agendamento/Entities/AddressEntity.java
@@ -1,12 +1,12 @@
 package com.saude.mais.agendamento.Entities;
 
+import com.saude.mais.agendamento.Dtos.AddressDto;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 @Entity
 @Table(name = "address")
@@ -14,27 +14,48 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
+@ToString
 public class AddressEntity implements Serializable {
     @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String street;
+    private String neighborhood;
+    private String number;
     private String city;
-    private String state;
+    private BrazilianStates state;
     private String zip;
 
 
-    @ManyToOne
-    @JoinColumn(name = "hospital_id")
-    private HospitalEntity hospital;
 
-
-    public AddressEntity(String street, String city, String state, String zip) {
+    public AddressEntity(String street, String neighborhood, String number, String city, BrazilianStates state, String zip) {
         this.street = street;
+        this.neighborhood = neighborhood;
+        this.number = number;
         this.city = city;
         this.state = state;
         this.zip = zip;
+    }
+
+    public AddressDto toAddressDto(){
+        return new AddressDto(getId(), getStreet(), getNeighborhood(), getNumber(), getCity(), getState(), getZip()).prettyData();
+    }
+
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        AddressEntity address = (AddressEntity) o;
+        return getId() != null && Objects.equals(getId(), address.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
