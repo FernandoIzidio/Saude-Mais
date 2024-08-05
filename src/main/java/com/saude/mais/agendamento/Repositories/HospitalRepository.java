1fd5eadd3ae56db488e879fbe66e5b638306b7c6
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 18:12:57 -0300

    Add methods for finding HospitalEntity by CNPJ, subdomain, and email

--- src/main/java/com/saude/mais/agendamento/Repositories/HospitalRepository.java
+++ src/main/java/com/saude/mais/agendamento/Repositories/HospitalRepository.java
@@ -3,5 +3,10 @@ package com.saude.mais.agendamento.Repositories;
 import com.saude.mais.agendamento.Entities.HospitalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HospitalRepository extends JpaRepository<HospitalEntity, Long> {
+    HospitalEntity findByCnpj(String cnpj);
+    HospitalEntity findBySubdomain(String subdomain);
+    HospitalEntity findByEmail(String email);
 }
