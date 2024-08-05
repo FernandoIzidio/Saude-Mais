b944f4b399737f9ef36b0498160a22155868a8a5
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-05 18:12:50 -0300

    Comment out findAllByHospitalId query in CustomerRepository

--- src/main/java/com/saude/mais/agendamento/Repositories/CustomerRepository.java
+++ src/main/java/com/saude/mais/agendamento/Repositories/CustomerRepository.java
@@ -1,7 +1,14 @@
 package com.saude.mais.agendamento.Repositories;
 
 import com.saude.mais.agendamento.Entities.CustomerEntity;
+import com.saude.mais.agendamento.Entities.WorkerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+//    @Query("SELECT c FROM CustomerEntity c WHERE c.user.hospital.id = :hospitalId")
+//    List<CustomerEntity> findAllByHospitalId(@Param("hospitalId") Long hospitalId);
 }
