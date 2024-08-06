8d57ed447031bf53f9ce700d16610c8ef740c43f
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-06 10:48:40 -0300

    Update: Enhanced UserService with user validation, authentication, and batch saving

--- src/main/java/com/saude/mais/agendamento/Services/UserService.java
+++ src/main/java/com/saude/mais/agendamento/Services/UserService.java
@@ -1,10 +1,22 @@
 package com.saude.mais.agendamento.Services;
 
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+import com.saude.mais.agendamento.Dtos.UserDto;
 import com.saude.mais.agendamento.Entities.User.UserEntity;
+import com.saude.mais.agendamento.Entities.User.UserRole;
+import com.saude.mais.agendamento.Repositories.HospitalRepository;
 import com.saude.mais.agendamento.Repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
 
@@ -12,15 +24,23 @@ import java.util.List;
 public class UserService {
 
     private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+    private HospitalRepository hospitalRepository;
+    private AddressService addressService;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, AuthenticationManager authenticationManager, HospitalRepository hospitalRepository, AddressService addressService) {
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.hospitalRepository = hospitalRepository;
+        this.addressService = addressService;
     }
 
     public List<UserEntity> findAll() {
         return userRepository.findAll();
     }
+
+
 
     public UserEntity findById(Long id) {
         return userRepository.findById(id).orElse(null);
@@ -30,11 +50,73 @@ public class UserService {
         return userRepository.findByCpf(cpf);
     }
 
-    public UserDetails findByEmail(String email) { return userRepository.findByEmail(email); }
-
-    public UserEntity save(UserEntity user) {
-        return userRepository.save(user);
+    public UserDetails findByUser(String username) {
+        return  userRepository.findByUser(username);
     }
+
+    public UserEntity findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+
+    public BindingResult validate(UserDto userDto, BindingResult bindingResult) {
+        if (findByUser(userDto.username()) != null){
+            bindingResult.rejectValue("userDto.username", "error.userDto", "Nome de usuário já cadastrado.");
+        }
+
+        if (userDto.userRole() != UserRole.ADMIN){
+            bindingResult.rejectValue("userDto.userRole", "error.userDto", "Erro ao processar formulário.");
+        }
+
+
+        if (findByEmail(userDto.email()) != null){
+            bindingResult.rejectValue("userDto.email", "error.userDto", "Email já cadastrado.");
+        }
+
+        if (findByCpf(userDto.cpf()) != null){
+            bindingResult.rejectValue("userDto.cpf", "error.userDto", "CPF já cadastrado.");
+
+        }
+
+        PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+        try {
+            Phonenumber.PhoneNumber numberProto = phoneNumberUtil.parse(userDto.phone(), "BR");
+            boolean isvalid = phoneNumberUtil.isValidNumber(numberProto) && phoneNumberUtil.getRegionCodeForNumber(numberProto).equals("BR");
+            if (!isvalid){
+                bindingResult.rejectValue("userDto.phone", "error.userDto", "Número de celular invalido.");
+            }
+
+        } catch (NumberParseException e) {
+            System.err.println("NumberParseException was thrown: " + e.toString());
+        }
+
+
+        return bindingResult;
+    }
+
+
+    @Transactional
+    public void save(UserEntity userEntity) {
+        userEntity.setPassword(new BCryptPasswordEncoder().encode(userEntity.getPassword()));
+        addressService.saveAll(userEntity.getAddresses());
+        userRepository.save(userEntity);
+    }
+
+    public void saveAll(List<UserEntity> userEntities){
+        userRepository.saveAll(userEntities);
+    }
+
+    public UserEntity getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (UserEntity) authentication.getPrincipal();
+    }
+
+
+    public void update(UserEntity userEntity){
+        addressService.saveAll(userEntity.getAddresses());
+        userRepository.save(userEntity);
+    }
+
 
     public void deleteById(Long id) {
         userRepository.deleteById(id);
