2ec11af7fa82e8105183b37834fca282b8211573
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-06 10:46:59 -0300

    Add: Added saveAll method to AddressService for batch saving

--- src/main/java/com/saude/mais/agendamento/Services/AddressService.java
+++ src/main/java/com/saude/mais/agendamento/Services/AddressService.java
@@ -6,6 +6,8 @@ import com.saude.mais.agendamento.Repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AddressService {
 
@@ -19,5 +21,9 @@ public class AddressService {
 
     public void save(AddressEntity address) {
         addressRepository.save(address);
+    }
+
+    public void saveAll(List<AddressEntity> addresses) {
+        addressRepository.saveAll(addresses);
     }
 }
