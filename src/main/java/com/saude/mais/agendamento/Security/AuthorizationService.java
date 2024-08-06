5091a2ab5be838bfee2e1e806ca41c24cb6ab54f
FernandoIzidio <luizfernando1010200@gmail.com>
2024-08-06 10:43:16 -0300

    Fix: Changed method call from findByEmail to findByUser in AuthorizationService

--- src/main/java/com/saude/mais/agendamento/Security/AuthorizationService.java
+++ src/main/java/com/saude/mais/agendamento/Security/AuthorizationService.java
@@ -19,7 +19,7 @@ public class AuthorizationService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username);
+        return userRepository.findByUser(username);
     }
 
 }
